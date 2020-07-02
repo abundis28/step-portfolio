1bdbc5e6574ff196b8fd2dac1dcdaa1c798ca255
Andrés Abundis <andresabundis@gmail.com>
2020-07-02 23:56:49 +0000

    Erases not used import.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -25,7 +25,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalDate;
 
 /** Servlet that returns a JSON string with quotes.
     TODO(aabundis): modify this file to handle comments data */
