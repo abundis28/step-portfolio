3de2c47eda2675dfe949a1c704b4c191fd9b24a9
Andrés Abundis <andresabundis@gmail.com>
2020-07-10 17:33:09 +0000

    Creates utility class with overloaded functions to convert differente objects to JSON.

---
+++ portfolio/src/main/java/com/google/sps/classes/UtilityClass.java
@@ -0,0 +1,42 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.classes;
+
+import com.google.gson.Gson;
+import com.google.sps.classes.UserAuthentication;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User object created to group login/logout url and status.
+ */
+public final class UtilityClass {
+  /**
+   * Converts ArrayList of comments to JSON using GSON class.
+   */
+  public static String convertToJsonUsingGson(List<String> comments) {
+    Gson gson = new Gson();
+    return gson.toJson(comments);
+  }
+
+  /**
+   * Overloaded convertToJsonUsingGson() function that converts user object 
+   * to JSON using GSON class.
+   */
+  public static String convertToJsonUsingGson(UserAuthentication user) {
+    Gson gson = new Gson();
+    return gson.toJson(user);
+  }
+}
